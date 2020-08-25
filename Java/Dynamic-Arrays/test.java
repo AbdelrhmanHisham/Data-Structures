d7730901d66edfa47f28c1190d543b2283385fb9
AbdelrhmanHisham <abdelrhman.hisham01@gmail.com>
2020-08-25 03:40:21 +0200

    abdo

---
+++ Java/Dynamic-Arrays/test.java
@@ -0,0 +1,25 @@
+public class test {
+
+private int health;
+private int move ;
+
+public test(int newhealth , int move)
+{
+health = newhealth;
+this.move = move;
+}    
+public test(){
+
+}
+
+public int gethealt() {
+    return health;
+}
+
+
+
+}
+
+
+
+
