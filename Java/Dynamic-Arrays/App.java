c8f8d55bf32b2dc49f30a98545907afb68c261f3
AbdelrhmanHisham <abdelrhman.hisham01@gmail.com>
2020-08-26 21:01:07 +0200

    Abdelrhman

--- Java/Dynamic-Arrays/App.java
+++ Java/Dynamic-Arrays/App.java
@@ -1,42 +1,24 @@
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class App {
 
- 
-    
-    public static void main(String[] args)
-    {        
- 
-        // int [] arr = new int [5];
-        // int [] new_arr = new int [10];
-        // Arrays.fill(arr,5);
-        // Arrays.fill(new_arr,10);
+  public static void main(String[] args){
 
-        // System.out.println(Arrays.toString(arr));
-        // arr = new_arr;
-        // System.out.println(Arrays.toString(arr));
-    
-        DynamicArrays list = new DynamicArrays();
-        list.add("shikaa");
-        // System.out.println(list.size()); 
-        // System.out.println(list.get_capacity());
-        // System.out.println(list.removeAt(0));
-        // System.out.println(list.get_capacity()); 
-        // System.out.println(list.size());
-        list.add("A");
-        list.add("V");
-        list.add("B");
-        list.remove("B");
-        // System.out.println(list.get_capacity()); 
-        // System.out.println(list.size());
-        list.print();
-    
-    }
-
-
-
-
+    // List list = new LinkedList();
+    DynamicArrays list = new DynamicArrays();
+    list.add("shikaa");
+    list.add(5);
+    list.add(10);
+    list.add(1.033);
+    list.add("hema");
+    Iterator it = list.iterator();
+    System.out.println(list.toString());
+    System.out.println(it.next());
+    System.out.println(it.next());
+    System.out.println(it.next());
+    System.out.println(it.next());
+  }
 }
-
-
-
--- Java/Dynamic-Arrays/DynamicArrays.java
+++ Java/Dynamic-Arrays/DynamicArrays.java
@@ -1,144 +1,175 @@
 @SuppressWarnings("unchecked")
-public class DynamicArrays<T>{
-    
-    T [] Darr;
-    int len =0;      // user-lenght 
-    private int capacity; // Acual capacity of the Dynamic array
+public class DynamicArrays<T> implements Iterable<T>{
+  T[] Darr;
+  int len = 0; // user-lenght
+  private int capacity; // Acual capacity of the Dynamic array
 
+  public int get_capacity() {
+    return this.capacity;
+  }
 
-    public int get_capacity(){
-        return this.capacity;
+  public DynamicArrays() {
+    this(10);
+  }
+
+  public DynamicArrays(int capacity) {
+    //check if the user enter a wrong capacity (must be > 0)
+    if (capacity < 0) throw new IllegalArgumentException(
+      "Illegal Capacity : " + capacity
+    );
+
+    this.capacity = capacity;
+    Darr = (T[]) new Object[capacity]; //create the array
+  }
+
+  public int size() {
+    return len;
+  }
+
+  
+  public T get(int index) {
+    if (index < 0 || index >= len) throw new IndexOutOfBoundsException();
+    return Darr[index];
+  }
+
+  
+  public void clear() {
+    for (int i = 0; i < len; i++) {
+      Darr[i] = null;
     }
-    public DynamicArrays(){
-        this(10);                 
+    len = 0;
+  }
+
+  
+  
+  public boolean isEmpty() {
+    if (len == 0) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
 
-    public DynamicArrays(int capacity)
-    {
-        //check if the user enter a wrong capacity (must be > 0)
-        if (capacity < 0) throw new IllegalArgumentException("Illegal Capacity : " + capacity);
+  public void set(int index, T element) {
+    if (index < 0 || index >= len) throw new IndexOutOfBoundsException();
+    Darr[index] = element;
+  }
 
-        this.capacity = capacity;
-        Darr = (T[]) new Object[capacity];  //create the array 
-    }
+  
+  
+  public void add(T element) {
+    if (len + 1 >= capacity) {
+      if (capacity == 0) {
+        capacity = 1;
+      } else {
+        capacity *= 2;
+      }
+      T[] new_Darr = (T[]) new Object[capacity]; //create a new one with double size
+      for (int i = 0; i < len; i++) {
+        new_Darr[i] = Darr[i];
+      }
 
-
-    public int size(){
-        return len;
-    }
-    
-
-    public T get(int index){
-        if (index < 0 || index >= len)throw new IndexOutOfBoundsException();
-        return Darr[index];
-    }
-    
-    public void clear(){
-        for (int i =0 ; i< len ; i++){
-            Darr[i] = null;
-        }
-        len = 0;
-    }
-    
-    public boolean isEmpty(){
-       if (len ==0){
-           return true;
-       }else{
-           return false;
-       }
-       
-    }
-
-    public void set(int index , T element){
-        if (index < 0 || index >= len)throw new IndexOutOfBoundsException();
-        Darr[index] = element; 
-
-    } 
-
-    public void add(T element)
-    {
-        if (len+1 >= capacity){
-            if (capacity ==0)
-            {
-                capacity=1;
-            }else{
-        capacity *=2;  
-            } 
-        T[] new_Darr = (T[])new Object[capacity]; //create a new one with double size
-        for (int i =0 ; i < len ; i++){
-            new_Darr[i] = Darr[i];
-        }
-
-        Darr = new_Darr;
-     
-        }
-
-        Darr[len++] = element;
+      Darr = new_Darr;
     }
 
-    public T removeAt(int index){
-        if (index < 0 || index >= len)throw new IndexOutOfBoundsException();
+    Darr[len++] = element;
+  }
 
-        T temp = Darr[index];
-        T[] new_Darr = (T[])new Object[len-1];
-        int j=0;
-        for (int i=0 ; i < len ; i++){
-            if (i == index){
-                j--;}
-                else{
-            new_Darr[j]=Darr[i];
-            j++;
-        }
-    
+  
+  
+  public T removeAt(int index) {
+    if (index < 0 || index >= len) throw new IndexOutOfBoundsException();
+
+    T temp = Darr[index];
+    T[] new_Darr = (T[]) new Object[len - 1];
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+      if (i == index) {
+        j--;
+      } else {
+        new_Darr[j] = Darr[i];
+        j++;
+      }
     }
     len--;
     capacity = len;
     Darr = new_Darr;
-return temp;
-    } 
+    return temp;
+  }
 
 
-    public boolean remove(Object obj){
 
+  public boolean remove(Object obj) {
     int index = indexOf(obj);
     boolean return_value;
-    if (index != -1){
-    removeAt(index);
+    if (index != -1) {
+      removeAt(index);
 
-    return_value = true;
-    }else {
-        return_value =false;
+      return_value = true;
+    } else {
+      return_value = false;
     }
 
     return return_value;
-    } 
- 
-    
-    public int indexOf(Object obj){
+  }
+
+  
+  public int indexOf(Object obj) {
     int index;
-    for (int i=0 ; i < len ; i++)
-    {
-    if (Darr[i].equals(obj)){
-        index  = i;
+    for (int i = 0; i < len; i++) {
+      if (Darr[i].equals(obj)) {
+        index = i;
         return index;
-    }}
+      }
+    }
 
     return -1;
-    
-}
+  }
 
- 
-    
-    
-public boolean contains(Object obj) {
+
+  public boolean contains(Object obj) {
     return indexOf(obj) != -1;
   }
 
 
-  public void print(){
-      for (int i=0 ; i< len ; i++){
-          System.out.println(Darr[i]);
+
+  public void print() {
+    for (int i = 0; i < len; i++) {
+      System.out.println(Darr[i]);
+    }
+  }
+
+
+  @Override
+  public java.util.Iterator<T> iterator() {
+    return new java.util.Iterator<T>() {
+      int index = 0;
+
+      @Override
+      public boolean hasNext() {
+        return index < len;
       }
+
+      @Override
+      public T next() {
+        return Darr[index++];
+      }
+
+      @Override
+      public void remove() {
+        throw new UnsupportedOperationException();
+      }
+    };
+  }
+
+  @Override
+  public String toString() {
+    if (len == 0) return "[]";
+    else {
+      StringBuilder sb = new StringBuilder(len).append("[");
+      for (int i = 0; i < len - 1; i++) sb.append(Darr[i] + ", ");
+      return sb.append(Darr[len - 1] + "]").toString();
+    }
   }
 }
