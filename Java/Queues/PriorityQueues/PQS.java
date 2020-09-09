5acb59e657fda6a73ed543832428753d360f4f11
AbdelrhmanHisham <abdelrhman.hisham01@gmail.com>
2020-09-09 16:16:22 +0200

    Abdelrhman

--- Java/Queues/PriorityQueues/PQS.java
+++ Java/Queues/PriorityQueues/PQS.java
@@ -7,6 +7,7 @@ import java.util.TreeSet;
 public class PQS<T extends Comparable<T>>{
     
     private int heapSize = 0;
+    private int Root = 0;
     private int heapCapacity = 0;
     private List<T> heap = null;
     private Map<T,TreeSet<Integer> > map = new HashMap<>(); 
@@ -17,6 +18,83 @@ public class PQS<T extends Comparable<T>>{
 
     public PQS(int sz){
         heap = new ArrayList<T>(sz);
+    }
+    
+    public PQS(T[] elements){
+        heapCapacity=heapSize = elements.length;
+        heap = new ArrayList<T>(heapCapacity);
+
+        for (int i=0 ; i<elements.length ; i++){
+            heap.add(elements[i]);
+        }
+
+
+        for(int parent = Math.max(0,(heapSize/2)-1) ; parent >= 0 ; parent--){
+            BubbleDown(parent);
+        }
+    }
+
+    public int size(){
+        return heapSize;
+    }
+    public void clear() {
+        for (int i = 0 ; i <heapSize ; i++){
+            heap.set(i,null);
+        }
+        heapSize=0;
+    }
+
+    public boolean isEmpty() {
+        return heapSize == 0;
+      }
+
+    public T peek() {
+        return heap.get(Root);
+    }  
+
+    private boolean less(int i,int j){
+        T node1 = heap.get(i);
+        T node2 = heap.get(j);
+
+        return node1.compareTo(node2) <=0;
+    } 
+
+
+    private void Swap(int i,int j){
+        T node1 = heap.get(i);
+        T node2 = heap.get(j);
+
+        heap.set(i,node2);
+        heap.set(j,node1);
+    } 
+
+    
+    private void BubbleDown(int parent_index){
+        while(true){
+            int right = 2*parent_index+1;
+            int left = 2*parent_index+2;
+            int smallest = left;
+            if (right < heapSize && less(right,left)){
+                smallest =right;
+            }
+            if(left >= heapSize ||less(parent_index,smallest)){
+                break;
+            }
+            Swap(parent_index , smallest);
+            parent_index = smallest;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return heap.toString();
+    } 
+    public static void main(String[] args) {
+        Integer [] arr = {10,2,3,50,100 , 0 , -1 , 50000 , -30};
+        PQS shikaa = new PQS(arr);
+        System.out.println(shikaa.toString());
+        shikaa.clear();
+        System.out.println(shikaa.toString());
     }
     
 }
